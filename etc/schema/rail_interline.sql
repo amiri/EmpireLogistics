6a0165b5b4fe2e6144c99e4ecb055b79e9cd8282
Amiri Barksdale at Home <amiri@roosterpirates.com>
2014-01-07 07:32:32 -0800

    All rail data--lines, nodes, interlines--importing
    Former-commit-id: 25ccd4242120d542a1b88bda09f43721eff2d039

---
+++ etc/schema/rail_interline.sql
@@ -0,0 +1,46 @@
+update raw_rail_interline set iidq = null where iidq = '';
+update raw_rail_interline set impedance = null where impedance = '';
+
+drop table rail_interline cascade;
+create table rail_interline (
+    id integer not null primary key,
+    interline_id_number integer,
+    forwarding_node text,
+    receiving_node text,
+    forwarding_node_owner text, 
+    receiving_node_owner text,
+    junction_code text,
+    impedance integer,
+    geometry geometry
+);
+insert into rail_interline (
+    id,
+    interline_id_number,
+    forwarding_node,
+    receiving_node,
+    forwarding_node_owner,
+    receiving_node_owner,
+    junction_code,
+    impedance,
+    geometry
+) select
+    ri.ogc_fid,
+    ri.iidq::integer,
+    rna.name,
+    rnb.name,
+    roa.name,
+    rob.name,
+    ri.iidname,
+    ri.impedance::integer,
+    ri.wkb_geometry
+from raw_rail_interline ri
+    left join rail_node rna
+        on ri.ija::integer = rna.junction_id
+    left join rail_node rnb
+        on ri.ijb::integer = rnb.junction_id
+    left join rail_ownership roa
+        on ri.wa = roa.reporting_mark
+    left join rail_ownership rob
+        on ri.wb = rob.reporting_mark
+;
+    
---
+++ etc/schema/rail_node.sql
@@ -0,0 +1,28 @@
+update raw_rail_node set jturn = null where jturn in ('-','_');
+update raw_rail_node set astate = null where astate in ('-','_');
+update raw_rail_node set splc = null where splc in ('-','_');
+update raw_rail_node set jname = 'Unknown' where jname in ('-','_');
+
+drop table if exists rail_node cascade;
+create table rail_node (
+    id integer not null primary key,
+    junction_id integer,
+    name text,
+    incident_links integer,
+    geometry geometry
+);
+insert into rail_node (
+    id,
+    junction_id,
+    name,
+    incident_links,
+    geometry
+) select
+    rn.gid as id,
+    rn.jid as junction_id,
+    rn.jname as name,
+    rn.incid as incident_links,
+    rn.geom as geometry
+from raw_rail_node rn
+;
+
