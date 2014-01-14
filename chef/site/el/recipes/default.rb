f9b507397c7d9450d8c5077ae8e43a2a99d8b0ae
Amiri Barksdale at Home <amiri@roosterpirates.com>
2014-01-14 14:30:53 -0800

    Fixed chef run, and fixed python lib path
    Former-commit-id: 2e369de4221869e99b5f293be28db46e49b7381c

--- chef/site/el/recipes/default.rb
+++ chef/site/el/recipes/default.rb
@@ -42,6 +42,12 @@ directory "/var/uwsgi" do
   action :create
 end
 
+%w{uwsgi uwsgi-app-integration-plugins uwsgi-core uwsgi-emperor uwsgi-extra uwsgi-infrastructure-plugins uwsgi-plugins-all}.each do |package|
+  apt_package package do
+    action :install
+  end
+end
+
 deploy_revision "empirelogistics" do
   repo "http://github.com/amiri/EmpireLogistics.git"
   deploy_to "/var/local/EmpireLogistics"
@@ -58,26 +64,27 @@ deploy_revision "empirelogistics" do
   symlink_before_migrate nil
   create_dirs_before_symlink   []
   purge_before_symlink         []
-  symlinks                      ({"python" => "python", "perl" => "perl", "logs" => "logs"})
+  symlinks                     nil
   scm_provider Chef::Provider::Git # is the default, for svn: Chef::Provider::Subversion
-  #notifies :restart, "service[uwsgi]"
+  notifies :restart, "service[uwsgi]"
 end
 
-#%w{uwsgi uwsgi-app-integration-plugins uwsgi-core uwsgi-emperor uwsgi-extra uwsgi-infrastructure-plugins uwsgi-plugins-all}.each do |package|
-  #apt_package package do
-    #action :install
-  #end
+include_recipe "python"
+
+execute "chown-python" do
+  command "chown -Rf el:el /var/local/EmpireLogistics/python"
+  action :run
+end
+
+#python_virtualenv "/var/local/EmpireLogistics/python" do
+  #interpreter "python2.7"
+  #owner "el"
+  #group "el"
+  #options "--system-site-packages"
+  #action :create
 #end
 
-python_virtualenv "/var/local/EmpireLogistics/python" do
-  interpreter "python2.7"
-  owner "el"
-  group "el"
-  options "--system-site-packages"
-  action :create
-end
-
-%w{uwsgi PIL https://github.com/migurski/modestmaps-py/archive/master.tar.gz simplejson werkzeug https://github.com/migurski/TileStache/archive/master.tar.gz}.each do |package|
+%w{PIL https://github.com/migurski/modestmaps-py/archive/master.tar.gz simplejson werkzeug https://github.com/migurski/TileStache/archive/master.tar.gz}.each do |package|
     python_pip package do
       virtualenv "/var/local/EmpireLogistics/python"
       action :install
@@ -85,9 +92,11 @@ end
     end
 end
 
-include_recipe "python"
-include_recipe "nginx"
-include_recipe "uwsgi::emperor"
+#execute "chown-python" do
+  #command "chown -Rf el:el /var/local/EmpireLogistics/python"
+  #action :run
+#end
+
 include_recipe "postgresql"
 include_recipe "postgresql::apt_repository"
 include_recipe "postgresql::postgis"
@@ -101,6 +110,8 @@ include_recipe "postgresql::client"
 include_recipe "postgresql::pg_user"
 include_recipe "postgresql::pg_database"
 include_recipe "postgresql::service"
+include_recipe "nginx"
+include_recipe "uwsgi::emperor"
 include_recipe "nodejs"
 include_recipe "npm"
 include_recipe "sudo"
@@ -116,6 +127,8 @@ cookbook_file "nginx.conf" do
   action :create_if_missing
 end
 
-service "nginx" do
-   action :restart
+%w{uwsgi nginx}.each do |service|
+  service service do
+    action :restart
+  end
 end
