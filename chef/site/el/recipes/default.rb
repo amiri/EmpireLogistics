8468991c867fae727393eb73b40fb5277229df98
Amiri Barksdale at Home <amiri@roosterpirates.com>
2014-01-14 23:35:49 -0800

    Remove libs from shared
    Former-commit-id: c0a3bcbc0758e06741d36bcdf9588a7dbc22f334

--- chef/site/el/recipes/default.rb
+++ chef/site/el/recipes/default.rb
@@ -60,7 +60,7 @@ deploy_revision "empirelogistics" do
   symlink_before_migrate nil
   create_dirs_before_symlink   []
   purge_before_symlink         []
-  symlinks                     ({"perl" => "perl", "python" => "python"})
+  symlinks                     nil
   scm_provider Chef::Provider::Git
   notifies :restart, "service[uwsgi]"
 end
