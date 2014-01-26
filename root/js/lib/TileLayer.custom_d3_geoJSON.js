06735cd0493eb012990d7db665b3871e4ca3d87f
root <root@li431-25.members.linode.com>
2014-01-26 22:37:16 +0000

    Cleanup
    Former-commit-id: f2aba1c418da98a1b80cf8eb862165cf9d2d28a7

--- root/js/lib/TileLayer.custom_d3_geoJSON.js
+++ root/js/lib/TileLayer.custom_d3_geoJSON.js
@@ -1,30 +1,56 @@
 L.TileLayer.custom_d3_geoJSON = L.TileLayer.extend({
-    onAdd: function(map) {
+    onAdd: function (map) {
         L.TileLayer.prototype.onAdd.call(this, map);
-        this._path = d3.geo.path().projection(function(d) {
+        this._path = d3.geo.path().projection(function (d) {
             var point = map.latLngToLayerPoint(new L.LatLng(d[1], d[0]));
             return [point.x, point.y];
         });
-        this.on("tileunload", function(d) {
+        this.on("tileunload", function (d) {
             if (d.tile.xhr) d.tile.xhr.abort();
             if (d.tile.nodes) d.tile.nodes.remove();
             d.tile.nodes = null;
             d.tile.xhr = null;
         });
     },
-    _loadTile: function(tile, tilePoint) {
+
+    _loadTile: function (tile, tilePoint) {
         var self = this;
-        console.log(self);
-        console.log(self.options);
         this._adjustTilePoint(tilePoint);
 
-        if (!tile.nodes && ! tile.xhr) {
-            tile.xhr = d3.json(this.getTileUrl(tilePoint), function(geoJson) {
+        if (!tile.nodes && !tile.xhr) {
+            tile.xhr = d3.json(this.getTileUrl(tilePoint), function (geoJson) {
                 tile.xhr = null;
                 tile.nodes = d3.select(map._container).select("svg").append("g");
-                tile.nodes.selectAll("path").data(geoJson.features).enter().append("path").attr("d", self._path).attr("class", self.options.class).attr("style", self.options.style);
+
+                if (self.options.type === "path") {
+                    tile.nodes.selectAll("path")
+                        .data(geoJson.features)
+                        .enter()
+                        .append(self.options.type)
+                        .attr("d", self._path)
+                        .attr("class", self.options.class)
+                        .attr("style", self.options.style);
+                }
+                if (self.options.type === "circle") {
+                    tile.nodes.selectAll("path")
+                        .data(geoJson.features)
+                        .enter()
+                        .append(self.options.type)
+                        .attr("d", self._path)
+                        .attr("cx", function (d) {
+                            var pixels = map.latLngToLayerPoint(new L.LatLng(d.geometry.coordinates[1], d.geometry.coordinates[0]));
+                            return pixels.x;
+                        })
+                        .attr("cy", function (d) {
+                            var pixels = map.latLngToLayerPoint(new L.LatLng(d.geometry.coordinates[1], d.geometry.coordinates[0]));
+                            return pixels.y;
+                        })
+                        .attr("class", self.options.class)
+                        .attr("r", self.options.radius)
+                        .attr("fill", self.options.fill);
+                }
+
             });
         }
     }
 });
-
