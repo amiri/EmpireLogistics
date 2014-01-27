4ec5f2820845151f496c3a8f8cae9fd2a5ca2667
root <root@li431-25.members.linode.com>
2014-01-27 02:07:09 +0000

    See if we can monkey-patch json datetime decoding
    Former-commit-id: 7826233aea461f34b066a51a0ad535bcfea1a7d8

--- empirelogistics_tiles.py
+++ empirelogistics_tiles.py
@@ -4,6 +4,17 @@ sys.path.append('/var/local/EmpireLogistics/shared/python/lib/python2.7/site-pac
 sys.path.append('/var/local/EmpireLogistics/shared/python/lib/python2.7/dist-packages')
 sys.path.append('/var/local/EmpireLogistics/shared/python/lib/python2.7/distutils')
 import os
+import json
+import datetime
+from time import mktime
+
+def new_default(self, obj):
+    if isinstance(obj, datetime.datetime):
+        return int(mktime(obj.timetuple()))
+
+    raise TypeError(repr(o) + " is not JSON serializable")
+
+json.JSONEncoder.default == new_default
 
 import TileStache
 application = TileStache.WSGITileServer('/var/local/EmpireLogistics/current/etc/empirelogistics_tiles.cfg')
