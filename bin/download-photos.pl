d390daef5a2894a84e4b54b21408e56bbea278b8
Amiri Barksdale at Home <amiri@roosterpirates.com>
2014-02-23 19:01:08 -0800

    Fix modulo in photo download
    Former-commit-id: def28ac0a24058aeba141b56b67e4202a2e97757 [formerly f55728c971d41ed50ba6a2f7c13d081751d75b01]
    Former-commit-id: 6358cf7dbb635f129452dc086f292b20bdd83e8b

--- bin/download-photos.pl
+++ bin/download-photos.pl
@@ -74,7 +74,7 @@ TYPE: for my $type ( sort @photo_types ) {
 
     # Loop through array of items of type
     ITEM: for my $item (@$items) {
-        sleep(86400) if $counter % 1000 = 0;
+        sleep(86400) if ($counter/1000 > 1) && $counter % 1000 == 0;
         $counter++;
         my $item_name = "lat_".$item->{latitude}."_lon_".$item->{longitude};
         $item_name =~ s/\./-/g;
