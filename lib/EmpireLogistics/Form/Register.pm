2134dfa80894b23071a387cc262629d7dcd6e421
Amiri Barksdale at Home <amiri@roosterpirates.com>
2014-04-03 07:18:25 -0700

    Fix validation
    Former-commit-id: 85828dfcc9d0d59a12add8d77407943cc20105ce [formerly eaab80f747e78cf93650f7b89336e1a9468de063]
    Former-commit-id: 3605c26d5b040348535480f51715ecf133eb357e

--- lib/EmpireLogistics/Form/Register.pm
+++ lib/EmpireLogistics/Form/Register.pm
@@ -1,66 +1,64 @@
 package EmpireLogistics::Form::Register;
 
 use HTML::FormHandler::Moose;
-use HTML::FormHandler::Types ('NoSpaces', 'Printable', 'NotAllDigits' );
+use HTML::FormHandler::Types ( 'NoSpaces', 'Printable', 'NotAllDigits' );
 use MooseX::Types::Common::String ('StrongPassword');
 use namespace::autoclean;
 extends 'EmpireLogistics::Form::Base';
 
-has '+name' => (default => 'register');
+has '+name' => ( default => 'register' );
 
 has 'schema' => (
-    is => 'ro',
+    is  => 'ro',
     isa => 'DBIx::Class::Schema',
 );
 
 has_field 'email' => (
-    type => 'Email',
+    type               => 'Email',
     email_valid_params => {
-        -mxcheck => 1,
+        -mxcheck  => 1,
         -tldcheck => 1,
     },
     required => 1,
 );
 has_field 'password' => (
-    type => 'Password',
-    apply => [ NoSpaces, Printable, NotAllDigits, StrongPassword ],
+    type      => 'Password',
+    apply     => [ NoSpaces, Printable, NotAllDigits, StrongPassword ],
     minlength => 8,
-    required => 1,
+    required  => 1,
 );
 has_field 'password_conf' => (
-    type => 'PasswordConf',
+    type     => 'PasswordConf',
     required => 1,
-    label => "Confirm Password",
+    label    => "Confirm Password",
 );
 has_field 'nickname' => (
-    type => 'Text',
+    type     => 'Text',
     required => 1,
-    label => "Display Name",
+    label    => "Display Name",
 );
 has_field 'submit' => (
-    type => 'Submit',
-    widget => 'ButtonTag',
-    value => 'Register',
-    element_class => ['btn','btn-primary'],
+    type          => 'Submit',
+    widget        => 'ButtonTag',
+    value         => 'Register',
+    element_class => [ 'btn', 'btn-primary' ],
 );
 
 sub validate_email {
-    my $self = shift;
-    my $count =
-      $self->form->schema->resultset('User')
-      ->search({email_address => $self->value,})->count;
+    my ( $self, $field ) = @_;
+    my $count = $self->form->schema->resultset('User')
+        ->search( { email => $field->value, } )->count;
     if ($count) {
-        $self->add_error("That email is already registered");
+        $field->add_error("That email is already registered");
     }
 }
 
 sub validate_nickname {
-    my $self = shift;
-    my $count =
-      $self->form->schema->resultset('User')
-      ->search({nickname => $self->value,})->count;
+    my ( $self, $field ) = @_;
+    my $count = $self->form->schema->resultset('User')
+        ->search( { nickname => $field->value, } )->count;
     if ($count) {
-        $self->add_error("That nickname is already taken");
+        $field->add_error("That nickname is already taken");
     }
 }
 
