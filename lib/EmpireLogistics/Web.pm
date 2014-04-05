c7e99e3ad4feb080a2151f413fae5ef1609003c3
Amiri Barksdale at Home <amiri@roosterpirates.com>
2014-04-05 10:07:59 -0700

    Logout long-lived cookie as well
    Former-commit-id: 55b3a084b5338b25e7286c143956b2033ef0438f [formerly 102db81bb1edf1ad336bfe29ce17113525717897]
    Former-commit-id: 9a8335e8e512523301e6df0c367c2f9f0a679cf7

--- lib/EmpireLogistics/Web.pm
+++ lib/EmpireLogistics/Web.pm
@@ -20,24 +20,42 @@ __PACKAGE__->apply_request_class_roles(
 __PACKAGE__->setup( @{$EmpireLogistics::Config::app_plugins} );
 
 around authenticate => sub {
-    my ($orig, $c) = (shift, shift);
+    my ( $orig, $c ) = ( shift, shift );
     my ($args) = @_;
 
-    my $remember    = delete $args->{remember};
+    my $remember = delete $args->{remember};
 
     my $user = $c->$orig(@_);
 
     # Store a login cookie for the user
     # if they've clicked "Remember me"
-    if ($user && $remember) {
+    if ( $user && $remember ) {
         $c->response->cookies->{login} = {
             value   => $user->encrypted_login_cookie,
             expires => '+36M',
-        }
+        };
     }
 
     return $user;
 };
+
+around logout => sub {
+    my ( $orig, $c ) = ( shift, shift );
+
+    $c->expire_cookie('login');
+    my $return = $c->$orig(@_);
+    return $return;
+};
+
+sub expire_cookie {
+    my ( $c, $cookie_name ) = @_;
+    return unless $c->req->cookies->{$cookie_name};
+    $c->res->cookies->{$cookie_name} = {
+        value   => $c->req->cookies->{$cookie_name}->value(),
+        expires => '-1d',
+    };
+    return 1;
+}
 
 __PACKAGE__->meta->make_immutable;
 
